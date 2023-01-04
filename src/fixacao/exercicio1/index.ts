2ec9175a5d65c3722d4e1a88d2862a8a2b5f8c24
Casenrique <carloshenriquesouza.eng@gmail.com>
2023-01-04 11:46:01 -0300

    Adicionar exercícios resolvidos aula

--- src/fixacao/exercicio1/index.ts
+++ src/fixacao/exercicio1/index.ts
@@ -9,3 +9,36 @@ que todos os objetos tenham as mesmas propriedades.
 
 3. Modifique o type Pessoa para que possamos escolher apenas entre as cores do arco-íris. Utilize um enum para isso.
 */
+
+enum RAINBOWCOLORS {
+  RED = "vermelha",
+  ORANGE = "laranja",
+  YELLOW = "amarela",
+  GREEN = "verde",
+  BLUE = "azul",
+  DARKBLUE = "azul escuro",
+  VIOLET = "violeta"
+}
+
+type TPerson = {
+  name: string,
+  age: number,
+  favoriteColor: RAINBOWCOLORS
+}
+
+const personOne: TPerson = {
+  name: "Thor",
+  age: 34,
+  favoriteColor: RAINBOWCOLORS.ORANGE
+} 
+const personTwo: TPerson = {
+  name: "Viúva Negra",
+  age: 27,
+  favoriteColor: RAINBOWCOLORS.DARKBLUE
+} 
+const personThree: TPerson = {
+  name: "Homem Aranha",
+  age: 16,
+  favoriteColor: RAINBOWCOLORS.RED
+} 
+
--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,47 @@ Crie um sistema de cadastro de usuários que contenha:
   b. NormalAccount
 */
 
+//1.1
 
+// export type TPerson = {
+//   id: string,
+//   name: string,
+//   email: string,
+//   password: string,
+//   role: "Admin" | "Normal"
+// }
 
+// const userOne:TPerson = {
+//   id: "01",
+//   name: "Carlos",
+//   email: "carlos@gmail.com",
+//   password: "12345",
+//   role: "Normal"
+// }
 
+// const userTwo:TPerson = {
+//   id: "02",
+//   name: "Henrique",
+//   email: "henrique@gmail.com",
+//   password: "@12345",
+//   role: "Admin"
+// }
 
+// console.table(userOne)
+// console.table(userTwo)
+// console.table([userOne, userTwo])
 
+//1.2
 
+export type TAdminAccount = {
+  account: string | number,
+  permission: boolean
+}
 
-
-
-
-
-
-
-
+export type TNormalAccount = {
+  account: string | number,
+  permission: boolean
+}
 
 /* PRÁTICA GUIADA - Parte 2
 Vamos continuar nosso sistema de cadastro de usuários criando:
@@ -32,3 +59,70 @@ Vamos continuar nosso sistema de cadastro de usuários criando:
 5. Guarde essas pessoas no array de usuários.
 
 */ 
+
+
+export enum ROLE {
+ ADMIN = "Admin",
+ NORMAL = "Normal"
+}
+
+export type TPerson = {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  role: ROLE
+}
+
+const userOne:TPerson = {
+  id: "01",
+  name: "Carlos",
+  email: "carlos@gmail.com",
+  password: "12345",
+  role: ROLE.NORMAL
+}
+
+const userTwo:TPerson = {
+  id: "02",
+  name: "Henrique",
+  email: "henrique@gmail.com",
+  password: "@12345",
+  role: ROLE.ADMIN
+}
+
+//2.2
+
+type TAdminUser = TPerson & TAdminAccount
+
+const userThree: TAdminUser = {
+  id: "03",
+  name: "Adão Negro",
+  email: "adaonegro@gmail.com",
+  password: "@12345!",
+  role: ROLE.ADMIN,
+  account: "admin1",
+  permission: true
+}
+
+type TNormalUser = TPerson & TNormalAccount
+
+const userFour: TNormalUser = {
+  id: "04",
+  name: "Gavião Arqueiro",
+  email: "gaviao_arqueiro@gmail.com",
+  password: "@12345!",
+  role: ROLE.NORMAL,
+  account: "normal1",
+  permission: false
+}
+
+console.table(userThree)
+console.table(userFour)
+console.table([userThree, userFour])
+
+//2.3
+
+const arrayUsers: TAdminUser[] | TNormalUser[] = []
+
+arrayUsers.push(userThree)
+arrayUsers.push(userFour)
